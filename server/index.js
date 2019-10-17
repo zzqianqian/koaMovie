7c6bea586b078532fbe1273fe031ca2a2a599631
zhangzhen <2385918066@qq.com>
2019-10-17 09:02:54 +0800

    添加gitignore

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
-const Koa=require('koa')
-const app=new Koa()
-const {normal}=require('./tpl')
-app.use(async (ctx,next)=>{
-    ctx.type='text/html;charset=utf-8'
-    ctx.body=normal
+const Koa = require('koa')
+const app = new Koa()
+const {normal} = require('./tpl')
+app.use(async (ctx, next) => {
+  ctx.type = 'text/html;charset=utf-8'
+  ctx.body = normal
 })
-
 
 app.listen(4455)
