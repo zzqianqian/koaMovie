4113ea8a89656d54676b4aed8c560664f19d001b
zhangzhen <2385918066@qq.com>
2019-10-23 17:43:57 +0800

    fdsafdsa

--- server/index.js
+++ server/index.js
@@ -6,10 +6,18 @@ app.use(views(path.join(__dirname, './views'), {
   extension: 'ejs'
 }))
 app.use(async (ctx, next) => {
-  let title = 'hello koa2 success'
-  await ctx.render('egsPlug', {
-    title
-  })
+  if (ctx.request.path === '/') {
+    let title = 'hello koa2 success'
+    await ctx.render('egsPlug', {
+      title
+    })
+  } else if (ctx.request.path === '/one') {
+    let title = 'hello koa2 success Two'
+    await ctx.render('1', {
+      title
+    })
+    //   ctx.response.body = 'list'
+  }
   await next()
 })
-app.listen(4455)
+app.listen(4465)
--- server/tpl/index.js
+++
@@ -1,6 +0,0 @@
-const normalTpl = require('./normal.js')
-const ejsTpl = require('./ejsTpl.ejs')
-module.exports = {
-  normal: normalTpl,
-  ejsTpl: ejsTpl
-}
